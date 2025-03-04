75f1c9e3f6d76c7355b57e2bb161a54fa5dd36c2
LeePiinheiro <lee.oficial@hotmail.com>
2025-03-04 19:22:05 -0300

    Add files via upload

---
+++ Projeto de Software/Diagrama_Ishikawa.mm
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+  <node TEXT="Falha na Integração dos Dados" FOLDED="false">
+    <node TEXT="Método">
+      <node TEXT="Falha no mapeamento dos requisitos"/>
+      <node TEXT="Processo de coleta mal definido"/>
+      <node TEXT="Falta de validação dos dados"/>
+    </node>
+    <node TEXT="Meio Ambiente">
+      <node TEXT="Conexão instável"/>
+      <node TEXT="Interferências climáticas"/>
+      <node TEXT="Diferença de temperatura"/>
+    </node>
+    <node TEXT="Medida">
+      <node TEXT="Inconsistência nas unidades"/>
+      <node TEXT="Falta de calibração dos sensores"/>
+      <node TEXT="Erros na inserção manual"/>
+    </node>
+    <node TEXT="Máquina">
+      <node TEXT="Falha nos sensores"/>
+      <node TEXT="Problemas de comunicação"/>
+      <node TEXT="Baixa capacidade de armazenamento"/>
+    </node>
+    <node TEXT="Material">
+      <node TEXT="Equipamentos desatualizados"/>
+      <node TEXT="Qualidade dos sensores"/>
+      <node TEXT="Uso de materiais inadequados"/>
+    </node>
+    <node TEXT="Mão de Obra">
+      <node TEXT="Falta de treinamento"/>
+      <node TEXT="Uso incorreto dos equipamentos"/>
+      <node TEXT="Erros na configuração"/>
+    </node>
+  </node>
+</map>
